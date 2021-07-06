0bb776686f5d99ad2eb32ee13db40f89d53c85a9
hardness1020 <hardness1020@gmail.com>
2021-07-06 16:59:13 +0800

    modify PBS

--- PBS/PBS.sh
+++ PBS/PBS.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-### queue name
+### queue name (跟系統要queue queue權限越高越先執行但可執行時間越短)
 #PBS -q large
 
 ### job name
@@ -10,11 +10,12 @@
 #PBS -l walltime=500:00:00,nodes=1:ppn=8
 
 ### error and output to certain files
-#PBS -e /home/hpc/cychen/user/linnil1/pbslog.e.txt
-#PBS -o /home/hpc/cychen/user/linnil1/pbslog.o.txt
+#PBS -e /home/hpc/cychen/user/{yourUserID}/pbslog.e.txt
+#PBS -o /home/hpc/cychen/user/{yourUserID}/pbslog.o.txt
 
-DIR=/home/hpc/cychen/user/linnil1/
+DIR=/home/hpc/cychen/user/{yourUserID}/
 
+# If there is any error in this script, then exit.
 set -e
 echo -e "Works start on $(date +"%T %D")\n"
 
@@ -25,14 +26,5 @@ python3 -c "import sys;print('first err log', file=sys.stderr)"
 echo "second log" >$DIR/log.o.txt
 python3 -c "import sys;print('second err log', file=sys.stderr)" 2>$DIR/log.e.txt
 
-bwa-mem
 
 echo "This line will not output bcz bwa-mem error"
-
-# How to use:
-# walltime = max running time
-# ppn = processes per node
-# save this script to test.pbs
-# qsub test.pbs
-# qstat
-# head *.txt

25da2068e18902ba368dbb5b51c8b63ecc7456cd
hardness1020 <hardness1020@gmail.com>
2021-07-06 17:04:33 +0800

    change PBS

---
+++ PBS/HelloWorld.py
@@ -0,0 +1,9 @@
+import numpy as np
+import time
+
+now = time.ctime()
+np1 = np.ones((3,3))
+
+print("Project: First PBS")
+print("Time: " + now)
+print(np1)
--- PBS/PBS.sh
+++ PBS/PBS.sh
@@ -13,18 +13,11 @@
 #PBS -e /home/hpc/cychen/user/{yourUserID}/pbslog.e.txt
 #PBS -o /home/hpc/cychen/user/{yourUserID}/pbslog.o.txt
 
-DIR=/home/hpc/cychen/user/{yourUserID}/
 
 # If there is any error in this script, then exit.
 set -e
 echo -e "Works start on $(date +"%T %D")\n"
 
 # do anything you want
-echo "first log"
-python3 -c "import sys;print('first err log', file=sys.stderr)"
-
-echo "second log" >$DIR/log.o.txt
-python3 -c "import sys;print('second err log', file=sys.stderr)" 2>$DIR/log.e.txt
-
-
-echo "This line will not output bcz bwa-mem error"
+echo "PBS"
+python3 HelloWorld.py

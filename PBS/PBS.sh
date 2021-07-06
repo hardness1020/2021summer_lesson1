155b71896b9124139edc0f87c5d79c011787c6d9
pibaobaobobo <k5847477@gmail.com>
2021-07-07 02:06:19 +0800

    Modify PBS

--- PBS/PBS.sh
+++ PBS/PBS.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-### queue name (跟系統要queue queue權限越高越先執行但可執行時間越短)
+### queue name 
 #PBS -q large
 
 ### job name
 #PBS -N your_job_name
 
 ### set number of nodes
-#PBS -l walltime=500:00:00,nodes=1:ppn=8
+#PBS -l walltime=50:00:00,nodes=1:ppn=8
 
 ### error and output to certain files
-#PBS -e /home/hpc/cychen/user/{yourUserID}/pbslog.e.txt
-#PBS -o /home/hpc/cychen/user/{yourUserID}/pbslog.o.txt
+#PBS -e {/home/hpc/cychen/2021_summer/PBS}/pbslog.e.txt
+#PBS -o {/home/hpc/cychen/2021_summer/PBS}/pbslog.o.txt
 
 
 # If there is any error in this script, then exit.
@@ -20,4 +20,4 @@ echo -e "Works start on $(date +"%T %D")\n"
 
 # do anything you want
 echo "PBS"
-python3 HelloWorld.py
+python3 {/home/hpc/cychen/2021_summer/PBS}/HelloWorld.py
